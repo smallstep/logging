ee910c8ef1d696e5aec96e14d3914ad100b05282
Herman Slatman <hermanslatman@hotmail.com>
2025-05-19 14:41:10 +0200

    Fix linting issues in examples

--- examples/httplog.go
+++ examples/httplog.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/smallstep/logging"
 	"github.com/smallstep/logging/httplog"
@@ -18,14 +19,15 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		fmt.Fprintln(w, "Hello World!")
 	})
 
 	srv := &http.Server{
-		Addr:     ":8080",
-		Handler:  httplog.Middleware(logger, http.DefaultServeMux),
-		ErrorLog: logger.StdLogger(logging.ErrorLevel),
+		Addr:              ":8080",
+		Handler:           httplog.Middleware(logger, http.DefaultServeMux),
+		ErrorLog:          logger.StdLogger(logging.ErrorLevel),
+		ReadHeaderTimeout: 30 * time.Second,
 	}
 
 	logger.Infof("start listening at %s.", srv.Addr)
--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module github.com/smallstep/logging
 
 go 1.23.0
 
-toolchain go1.23.7
+toolchain go1.23.8
 
 require (
 	github.com/pkg/errors v0.9.1
